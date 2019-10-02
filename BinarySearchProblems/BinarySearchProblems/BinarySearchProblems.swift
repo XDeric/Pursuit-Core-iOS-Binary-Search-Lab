e247e5090c43ec98d970f956af86c676b5f16777
eeeric3995@yahoo.com <ihxx2s7tbu0>
2019-10-02 16:23:05 -0400

    3 was given but finished 1 and 2

--- BinarySearchProblems/BinarySearchProblems/BinarySearchProblems.swift
+++ BinarySearchProblems/BinarySearchProblems/BinarySearchProblems.swift
@@ -5,13 +5,36 @@ import Foundation
 // 1. Find if an element is contained within a sorted array in O(log(n)) time.  Do not use recursion.
 
 func binarySearch<T: Comparable>(arr: [T], target: T) -> Bool {
+    var minIndex = 0
+    var maxIndex = arr.count
+    while minIndex < maxIndex {
+        let middleIndex = (minIndex + maxIndex) / 2
+        print(middleIndex)
+        if arr[middleIndex] == target {
+            return true
+        }else if target < arr[middleIndex]{
+            maxIndex = middleIndex - 1
+            print(maxIndex)
+        }else if target > arr[middleIndex]{
+            minIndex = middleIndex + 1
+            print(minIndex)
+        }
+    }
     return false
 }
 
 // 2. Find if an element is contained within a sorted array in O(log(n)) time.  Use recursion.
 
 func recursiveBinarySearch<T: Comparable>(arr: [T], target: T) -> Bool {
-    return false
+    var minIndex = 0
+    var maxIndex = arr.count - 1
+    let middleIndex = (minIndex + maxIndex) / 2
+    if target == arr[middleIndex]{
+        return true
+    } else if target < arr[middleIndex] {
+        return binarySearch(arr: arr, target: target)
+    } else {
+        return binarySearch(arr: arr, target: target) }
 }
 
 // https://www.interviewbit.com/problems/matrix-search/
@@ -22,7 +45,20 @@ func recursiveBinarySearch<T: Comparable>(arr: [T], target: T) -> Bool {
 // The first integer of each row is greater than or equal to the last integer of the previous row.
 
 func contains(value: Int, in arr: [[Int]]) -> Bool {
-    return false
+    var minIndex = 0
+    var maxIndex = arr.count - 1
+    while minIndex < maxIndex {
+        let middleIndex = (minIndex + maxIndex) / 2
+        if arr[middleIndex][0] == value {
+            return true
+        }else if value < arr[middleIndex][0]{
+            maxIndex = middleIndex - 1
+        }else if value > arr[middleIndex][0]{
+            minIndex = middleIndex + 1
+        }
+    }
+    let targetArr = arr[minIndex]
+    return recursiveBinarySearch(arr: targetArr, target: value)
 }
 
 
@@ -31,6 +67,8 @@ func contains(value: Int, in arr: [[Int]]) -> Bool {
 // 4. Given a sorted array of integers, find the index of the last occurrence of a given number.  Your solution should work in O(log(n)) time
 
 func lastOccurrence<T: Comparable>(of value: T, in arr: [T]) -> Int? {
+    
+    
     return nil
 }
 
